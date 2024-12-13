0b5b5f59143ed685979a410c6a57e8761f7feb9d
Marat Taktarov <georgebauman@yandex.ru>
2024-12-13 21:50:41 +0300

    Started implementing PNG writer

--- include/png_writer.h
+++ include/png_writer.h
@@ -1,1 +1,10 @@
 #pragma once
+
+#include <cstdint>
+#include <vector>
+#include <string>
+
+class PNGWriter {
+public:
+    static void WritePNG(const std::string& filename, uint64_t width, uint64_t height, const std::vector<uint8_t>& compressed_data);
+};
