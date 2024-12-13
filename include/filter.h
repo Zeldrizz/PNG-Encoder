6ce6b4965fe040c9a52bc79b12c91e8ff15756a6
Marat Taktarov <georgebauman@yandex.ru>
2024-12-13 10:38:28 +0300

    PNGFilter in filter.h

--- include/filter.h
+++ include/filter.h
@@ -1,1 +1,19 @@
 #pragma once
+
+#include <cstdint>
+#include <vector>
+
+enum class PNGFilterType {
+    None = 0,
+    Sub = 1,
+    Up = 2,
+    Average = 3,
+    Paeth = 4
+};
+
+class PNGFilter {
+public:
+    std::vector<uint8_t> Apply(const std::vector<uint8_t>& rgb_data, uint64_t width, uint64_t height);
+private:
+    uint8_t PaethPredictor(uint8_t a, uint8_t b, uint8_t c);
+};
